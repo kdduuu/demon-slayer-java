25b712c41e356913851e58218cf0347603c088ac
Kadu Almeida <monicaekadu2@gmail.com>
2026-08-13 22:02:06 -0300

    refactor: remove idade de Personagem

--- src/main/java/br/com/demonslayer/Personagem.java
+++ src/main/java/br/com/demonslayer/Personagem.java
@@ -3,10 +3,8 @@ package br.com.demonslayer;
 public class Personagem {
 
     public String nome;
-    public int idade;
 
     public void exibirInformacoes() {
         System.out.println("Nome: " + nome);
-        System.out.println("Idade: " + idade);
     }
 }
