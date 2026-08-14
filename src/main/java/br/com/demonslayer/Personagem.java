4221b4911f037fd52930d4aea635d510c61c1b03
Kadu Almeida <monicaekadu2@gmail.com>
2026-08-13 21:36:17 -0300

    feat: cria classe base Personagem

---
+++ src/main/java/br/com/demonslayer/Personagem.java
@@ -0,0 +1,16 @@
+package br.com.demonslayer;
+
+public class Personagem {
+    package br.com.demonslayer;
+
+    public class Personagem {
+
+        public String nome;
+        public int idade;
+
+        public void exibirInformacoes() {
+            System.out.println("Nome: " + nome);
+            System.out.println("Idade: " + idade);
+        }
+    }
+}
