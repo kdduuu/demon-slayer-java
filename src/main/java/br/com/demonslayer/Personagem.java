9bc08cda68540f508533198abb38917e9a22704b
Kadu Almeida <monicaekadu2@gmail.com>
2026-08-13 21:42:02 -0300

    fix: corrige estrutura da classe Personagem

--- src/main/java/br/com/demonslayer/Personagem.java
+++ src/main/java/br/com/demonslayer/Personagem.java
@@ -1,16 +1,12 @@
 package br.com.demonslayer;
 
 public class Personagem {
-    package br.com.demonslayer;
 
-    public class Personagem {
+    public String nome;
+    public int idade;
 
-        public String nome;
-        public int idade;
-
-        public void exibirInformacoes() {
-            System.out.println("Nome: " + nome);
-            System.out.println("Idade: " + idade);
-        }
+    public void exibirInformacoes() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
     }
 }
