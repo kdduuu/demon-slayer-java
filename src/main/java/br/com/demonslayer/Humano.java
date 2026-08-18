b1e4b73f2dd6434a90b7cd755069918423640145
Kadu Almeida <monicaekadu2@gmail.com>
2026-08-18 20:37:03 -0300

    feat: adiciona construtores em Humano

--- src/main/java/br/com/demonslayer/Humano.java
+++ src/main/java/br/com/demonslayer/Humano.java
@@ -2,4 +2,11 @@ package br.com.demonslayer;
 
 public class Humano extends Personagem {
 
+    public Humano() {
+        super();
+    }
+
+    public Humano(String nome) {
+        super(nome);
+    }
 }
