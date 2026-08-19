90238755690c00629302753242ffeaddc0bd44c8
Kadu Almeida <monicaekadu2@gmail.com>
2026-08-18 21:07:07 -0300

    feat: cria classe base CacadorDeOnis

---
+++ src/main/java/br/com/demonslayer/CacadorDeOnis.java
@@ -0,0 +1,12 @@
+package br.com.demonslayer;
+
+public class CacadorDeOnis extends Humano {
+
+    public CacadorDeOnis() {
+        super();
+    }
+
+    public CacadorDeOnis(String nome) {
+        super(nome);
+    }
+}
