d569a50685b90aa6cb2b5af48cbc83af6e8e800a
FehAlmeida1406 <fernandoafernandes1406@gmail.com>
2026-08-18 21:07:17 -0300

    feat: cria classe base DozeKizuki

---
+++ src/main/java/br/com/demonslayer/DozeKizuki.java
@@ -0,0 +1,12 @@
+package br.com.demonslayer;
+
+public class DozeKizuki extends Oni {
+
+    public DozeKizuki() {
+        super();
+    }
+
+    public DozeKizuki(String nome) {
+        super(nome);
+    }
+}
