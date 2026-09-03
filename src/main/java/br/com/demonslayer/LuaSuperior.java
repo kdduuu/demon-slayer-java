6f5eaecb13084a21bdcbb34d7ffb8b2e0a136632
FehAlmeida1406 <fernandoafernandes1406@gmail.com>
2026-09-02 22:40:39 -0300

    feat: cria classe base LuaSuperior

--- src/main/java/br/com/demonslayer/LuaSuperior.java
+++ src/main/java/br/com/demonslayer/LuaSuperior.java
@@ -1,4 +1,12 @@
 package br.com.demonslayer;
 
-public class LuaSuperior {
+public class LuaSuperior extends DozeKizuki {
+
+    public LuaSuperior() {
+        super();
+    }
+
+    public LuaSuperior(String nome) {
+        super(nome);
+    }
 }
